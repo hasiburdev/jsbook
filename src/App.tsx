dc9486a686c0e828d98f88080e9119d095e46158
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-03-04 23:59:45 +0600

    Add tiny-test-pkg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+  const ref = useRef<any>();
 
-  const onClick = () => {
-    console.log(input);
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
   };
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+    console.log(result);
+    setCode(result.outputFiles[0].text);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   return (
     <div>
@@ -16,7 +40,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre></pre>
+      <pre>{code}</pre>
     </div>
   );
 };
