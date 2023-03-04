50f0ed9c86cb22de878fcb161f62f3d8c6e72e6a
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-03-04 12:13:02 +0600

    Starting from zero again

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,24 @@
-import * as esbuid from "esbuild-wasm";
-import { useEffect, useRef, useState } from "react";
-
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-
-function App() {
+import { useState } from "react";
+const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const ref = useRef<any>();
-
-  const startService = async () => {
-    ref.current = await esbuid.startService({
-      worker: true,
-      wasmURL: "/esbuild.wasm",
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const onClick = async () => {
-    if (!ref.current) return;
-
-    // const result = await ref.current.transform(input, {
-    //   loader: "jsx",
-    //   target: "es2015",
-    // });
-
-    const result = await ref.current.build({
-      bundle: true,
-      write: false,
-      entryPoints: ["index.js"],
-      plugins: [unpkgPathPlugin()],
-    });
-
-    console.log(result.outputFiles[0].text);
-    setCode(result.outputFiles[0].text);
+  const onClick = () => {
+    console.log(input);
   };
 
   return (
     <div>
       <textarea
-        onChange={(e) => setInput(e.target.value)}
         value={input}
+        onChange={(e) => setInput(e.target.value)}
       ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <pre></pre>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode as HTMLElement);
+
+root.render(<App />);
