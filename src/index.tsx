3e2524f5825ecd78e55faf8ec8f6b6e1049d1d79
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-03-17 17:55:48 +0600

    Add direction icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Provider } from "react-redux";
 import { store } from "./state";
 
